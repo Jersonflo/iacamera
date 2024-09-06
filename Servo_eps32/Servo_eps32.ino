5132e0250bbc52aed5ce4157055c5f3e980ebfee
EnigmaEcho <jgiraldof@unal.edu.co>
2024-09-06 10:36:37 -0500

    Update Servo_eps32.ino

--- Servo_eps32/Servo_eps32.ino
+++ Servo_eps32/Servo_eps32.ino
@@ -12,13 +12,14 @@ Servo myServo_1;
 void setup() {
   myServo.attach(servoPin);
   myServo_1.attach(servoPin_1);
-  myServo.write(90);  // Detiene ambos servos al inicio
+  myServo.write(90);  // Posicionar los servos en 90 grados al iniciar el código
   myServo_1.write(90);
   Serial.begin(9600);
   Serial.setTimeout(10);  // Ajustar el tiempo de espera para Serial
 }
 
 void loop() {
+  
   while (Serial.available()) {
     dato = Serial.read();
     delay(10);
@@ -27,26 +28,26 @@ void loop() {
     switch(dato) {
 
        case 'd': 
-        velocidad = velocidad + 6;
+        velocidad = velocidad + 8;
         if (velocidad > 180) velocidad = 180;  // Limitar ángulo máximo
         myServo.write(velocidad);
         break;
        
 
       case 'i': 
-        velocidad = velocidad - 6;  // Incrementa la velocidad en la dirección opuesta
+        velocidad = velocidad - 8;  // Incrementa la velocidad en la dirección opuesta
         if (velocidad < 0) velocidad = 0;  // Velocidad máxima en la otra dirección
         myServo.write(velocidad);
         break;
-
+    
       case 'a': 
-        velocidad = velocidad + 6;  // Incrementa la velocidad en una dirección
+        velocidad = velocidad + 12;  // Incrementa la velocidad en una dirección
         if (velocidad > 180) velocidad = 180;
         myServo_1.write(velocidad);
         break;
 
       case 'b': 
-        velocidad = velocidad - 6;  // Incrementa la velocidad en la dirección opuesta
+        velocidad = velocidad - 12;  // Incrementa la velocidad en la dirección opuesta
         if (velocidad < 0) velocidad = 0;
         myServo_1.write(velocidad);
         break;
