3b4066ebd08605c5cb7e553bb1d511d74b905386
EnigmaEcho <jgiraldof@unal.edu.co>
2024-09-06 15:17:12 -0500

    Update Servo_eps32.ino

--- Servo_eps32/Servo_eps32.ino
+++ Servo_eps32/Servo_eps32.ino
@@ -16,7 +16,7 @@ void setup() {
   myServo.write(90);  // Posicionar los servos en 90 grados al iniciar el código
   myServo_1.write(90);
   Serial.begin(9600);
-  Serial.setTimeout(10);  // Ajustar el tiempo de espera para Serial
+  Serial.setTimeout(5);  // Ajustar el tiempo de espera para Serial
 }
 
 void loop() {
@@ -29,36 +29,36 @@ void loop() {
     switch(dato) {
       // Movimiento horizontal a la derecha
       case 'd': 
-        velocidad_h = velocidad_h + 10;
+        velocidad_h = velocidad_h + 12;
         if (velocidad_h > 180) velocidad_h = 180;  // Limitar ángulo máximo
         myServo.write(velocidad_h);
         break;
        
       // Movimiento horizontal a la izquierda
       case 'i': 
-        velocidad_h = velocidad_h - 10;  // Incrementa la velocidad en la dirección opuesta
+        velocidad_h = velocidad_h - 12;  // Incrementa la velocidad en la dirección opuesta
         if (velocidad_h < 0) velocidad_h = 0;  // Limitar ángulo mínimo
         myServo.write(velocidad_h);
         break;
     
       // Movimiento vertical hacia arriba
       case 'a': 
-        velocidad_v = velocidad_v + 10;
+        velocidad_v = velocidad_v + 16;
         if (velocidad_v > 180) velocidad_v = 180;  // Limitar ángulo máximo
         myServo_1.write(velocidad_v);
         break;
 
       // Movimiento vertical hacia abajo
       case 'b': 
-        velocidad_v = velocidad_v - 10;
+        velocidad_v = velocidad_v - 16;
         if (velocidad_v < 0) velocidad_v = 0;  // Limitar ángulo mínimo
         myServo_1.write(velocidad_v);
         break;
 
         // Movimiento diagonal izquierda-arriba
       case '1':  // Nueva etiqueta para el caso 'i' + 'a'
-        velocidad_h = velocidad_h - 10;
-        velocidad_v = velocidad_v + 10;
+        velocidad_h = velocidad_h - 12;
+        velocidad_v = velocidad_v + 16;
         if (velocidad_h < 0) velocidad_h = 0;
         if (velocidad_v > 180) velocidad_v = 180;
         myServo.write(velocidad_h);
@@ -67,8 +67,8 @@ void loop() {
 
       // Movimiento diagonal derecha-arriba
       case '2':  // Nueva etiqueta para el caso 'd' + 'a'
-        velocidad_h = velocidad_h + 10;
-        velocidad_v = velocidad_v + 10;
+        velocidad_h = velocidad_h + 12;
+        velocidad_v = velocidad_v + 16;
         if (velocidad_h > 180) velocidad_h = 180;
         if (velocidad_v > 180) velocidad_v = 180;
         myServo.write(velocidad_h);
@@ -77,8 +77,8 @@ void loop() {
 
       // Movimiento diagonal izquierda-abajo
       case '3':  // Nueva etiqueta para el caso 'i' + 'b'
-        velocidad_h = velocidad_h - 10;
-        velocidad_v = velocidad_v - 10;
+        velocidad_h = velocidad_h - 12;
+        velocidad_v = velocidad_v - 16;
         if (velocidad_h < 0) velocidad_h = 0;
         if (velocidad_v < 0) velocidad_v = 0;
         myServo.write(velocidad_h);
@@ -87,8 +87,8 @@ void loop() {
 
       // Movimiento diagonal derecha-abajo
       case '4':  // Nueva etiqueta para el caso 'd' + 'b'
-        velocidad_h = velocidad_h + 10;
-        velocidad_v = velocidad_v - 10;
+        velocidad_h = velocidad_h + 12;
+        velocidad_v = velocidad_v - 16;
         if (velocidad_h > 180) velocidad_h = 180;
         if (velocidad_v < 0) velocidad_v = 0;
         myServo.write(velocidad_h);
